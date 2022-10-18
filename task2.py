df976e77267dc83537c231ebe4b263380d53c9bb
Irina-Ledneva <IrinaLedneva123@yandex.ru>
2022-10-18 12:30:10 +0300

    fghfj

--- task2.py
+++ task2.py
@@ -10,5 +10,5 @@ def my_func(name, surname, year, city, telephone):
     return " ".join([name, surname, year, city, email, telephone])
 
 
-print(my_func(name="Irina", surname="Ledneva", year="1945", city="world", email="error404@mail.ru",
-              telephone="not found"))
+print(my_func(name=name, surname=surname, year=year, city=city, email=email,
+              telephone=telephone))
